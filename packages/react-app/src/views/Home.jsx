b05d90566e6985ecb31fde67ab6adc215f4bca3a
G2 <bracy.gawain2014@gmail.com>
2022-07-12 23:41:31 -0400

    Initialize poets-and-poems

---
+++ packages/react-app/src/views/Home.jsx
@@ -0,0 +1,10 @@
+// import { useContractReader } from "eth-hooks";
+// import { ethers } from "ethers";
+import React from "react";
+// import { Link } from "react-router-dom";
+
+function Home({ readContracts }) {
+  return <div>Hello Sample Poet & Poems</div>;
+}
+
+export default Home;
